9ba8e031050f00310c60ec6225f10d075c90270b
Gading Sobad <gading.rengga@gmail.com>
2022-07-19 15:30:40 +0700

    Update Config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/internship-new/';
+$config['base_url'] = 'https://s.soloabadi.com/system-internship/';
 
 /*
 |--------------------------------------------------------------------------
